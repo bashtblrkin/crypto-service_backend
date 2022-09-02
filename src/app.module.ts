827e015e1bd8924467d8d00c685dabc8bd7c70af
атурин ван <baturin.i@asbc.ru>
2022-09-02 15:10:13 +0300

    upd main module

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,30 @@
-import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import { Module } from '@nestjs/common'
+import { AppController } from './app.controller'
+import { AppService } from './app.service'
+import { ConfigModule, ConfigService } from '@nestjs/config'
+import { SequelizeModule } from '@nestjs/sequelize'
+import { getSequelizeConfig } from './config/sequilize.config'
+import { UserModule } from './user/user.module';
+import { WalletModule } from './wallet/wallet.module';
+import { CurrencyModule } from './currency/currency.module';
+import { OrderModule } from './order/order.module';
+import { AuthModule } from './auth/auth.module';
 
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+	imports: [
+		ConfigModule.forRoot(),
+		SequelizeModule.forRootAsync({
+			imports: [ConfigModule],
+			inject: [ConfigService],
+			useFactory: getSequelizeConfig
+		}),
+		UserModule,
+		CurrencyModule,
+		WalletModule,
+		OrderModule,
+		AuthModule
+	],
+	controllers: [AppController],
+	providers: [AppService]
 })
 export class AppModule {}
